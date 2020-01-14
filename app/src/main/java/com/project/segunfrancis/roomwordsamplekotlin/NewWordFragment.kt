c5f1a13d5ea588b880c0adef71a5e28f4c669d13
segunfrancis <francis4dblues@gmail.com>
2020-01-14 16:07:25 +0100

    nug fix

--- app/src/main/java/com/project/segunfrancis/roomwordsamplekotlin/NewWordFragment.kt
+++ app/src/main/java/com/project/segunfrancis/roomwordsamplekotlin/NewWordFragment.kt
@@ -1,13 +1,14 @@
 package com.project.segunfrancis.roomwordsamplekotlin
 
 
+import android.app.Activity
 import android.os.Bundle
 import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.view.inputmethod.InputMethodManager
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_new_word.*
@@ -15,6 +16,7 @@ import kotlinx.android.synthetic.main.fragment_new_word.*
 /**
  * A simple [Fragment] subclass.
  */
+
 class NewWordFragment : Fragment() {
 
     override fun onCreateView(
@@ -32,15 +34,29 @@ class NewWordFragment : Fragment() {
                 val bundle = Bundle()
                 bundle.putString(EXTRA_REPLY, word)
                 findNavController().navigate(R.id.action_newWordFragment_to_mainFragment, bundle)
+                hideSoftKeyboard(requireActivity())
                 Snackbar.make(view, "Word has been saved", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
             } else {
-                Toast.makeText(requireContext(), "Empty Note", Toast.LENGTH_SHORT).show()
+                Snackbar.make(view, "Cannot save empty note", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
             }
         }
     }
 
     companion object {
         const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
+    }
+
+    private fun hideSoftKeyboard(activity: Activity) {
+        val inputMethodManager =
+            requireActivity().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        var view = activity.currentFocus
+        if (view != null) {
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        } else {
+            view = View(activity)
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
